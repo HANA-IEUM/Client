0421e20e7121e0da83c7d8a59fea56905444fc64
dbstj0403 <wcn09088@naver.com>
2025-08-25 09:52:17 +0900

    style: ant design으로 변경

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,22 @@
-import { Button } from '@/components/ui/button';
+import { Button } from 'antd';
 
 const LandingPage = () => {
   return (
     <div>
       <h1 className="font-hana-medium">랜딩 페이지입니다.</h1>
-      <Button>기본 버튼</Button>
-      <Button variant="outline">아웃라인 버튼</Button>
-      <Button variant="destructive">위험 버튼</Button>
-      <button className="bg-theme-primary">하나그린 버튼</button>
+      <Button>Default</Button>
+
+      {/* 기본 제공 프라이머리 */}
+      <Button type="primary">Primary</Button>
+
+      {/* 위험(빨간색) 버튼 */}
+      <Button danger>Danger</Button>
+
+      {/* 점선 스타일 */}
+      <Button type="dashed">Dashed</Button>
+
+      {/* 링크 버튼 */}
+      <Button type="link">Link</Button>
     </div>
   );
 };
