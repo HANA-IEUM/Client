fe23612c3de8cb7e92a816f429fdee34233eb2f3
dbstj0403 <wcn09088@naver.com>
2025-08-25 13:55:27 +0900

    feat: 계좌 아이템 컴포넌트 구현 (#4)

---
+++ src/components/AccountItem.tsx
@@ -0,0 +1,33 @@
+import type { AccountItemProps } from '@/types/common';
+import CheckIcon from '@/assets/common/CheckIcon';
+import HanaIcon from '@/assets/common/HanaIcon';
+
+const AccountItem = ({
+  accountName = '달달 하나 통장',
+  accountNum = '352-1022-1234-12',
+  selected = false,
+  onClick,
+}: AccountItemProps) => {
+  return (
+    <div
+      className={`w-80 h-[66px] flex justify-between items-center cursor-pointer py-2 pl-3 pr-4 shadow-sm rounded-md transition-colors
+        ${selected ? 'bg-accent-secondary' : 'bg-btn-default-bg'}`}
+      onClick={onClick}
+    >
+      <div className="flex justify-center items-center">
+        <HanaIcon />
+      </div>
+      <div className="flex flex-col">
+        <span className="font-hana-bold text-primary text-xl">
+          {accountName}
+        </span>
+        <span className="font-hana-regular text-lg">{accountNum}</span>
+      </div>
+      <div className="flex justify-center items-center">
+        <CheckIcon selected={selected} />
+      </div>
+    </div>
+  );
+};
+
+export default AccountItem;
