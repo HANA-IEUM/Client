a7c0312428080791bbfb951543c128e86e15f7d8
dbstj0403 <wcn09088@naver.com>
2025-08-25 13:06:09 +0900

    feat: CallIcon 추가 (#4)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import CallIcon from '@/assets/common/CallIcon';
+
 const HomePage = () => {
-  return <div>홈 페이지입니다</div>;
+  return (
+    <div>
+      홈 페이지입니다.
+      <CallIcon />
+    </div>
+  );
 };
 
 export default HomePage;
