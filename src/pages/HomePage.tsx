ba52a7a280a390331a7e5d22ff96446f57d09dd6
dbstj0403 <wcn09088@naver.com>
2025-08-25 13:55:27 +0900

    feat: 계좌 아이템 컴포넌트 구현 (#4)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import CallIcon from '@/assets/common/CallIcon';
+import AccountItem from '@/components/AccountItem';
 
 const HomePage = () => {
+  const [selected, setSelected] = useState(false);
   return (
     <div>
       홈 페이지입니다.
       <CallIcon />
+      <AccountItem
+        accountName="달달 하나 통장"
+        accountNum="352-1022-1234-12"
+        selected={selected}
+        onClick={() => setSelected((v) => !v)}
+      />
     </div>
   );
 };
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,10 @@
 export type IconProps = {
   active?: boolean;
 };
+
+export type AccountItemProps = {
+  accountName?: string;
+  accountNum?: string;
+  selected?: boolean;
+  onClick: () => void;
+};
