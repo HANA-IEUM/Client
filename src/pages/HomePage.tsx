48c6a2743f9021649276db81fe5cb68632f75af0
dbstj0403 <wcn09088@naver.com>
2025-08-25 14:27:58 +0900

    feat: 버킷리스트 선택 컴포넌트 구현 (#4)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CallIcon from '@/assets/common/CallIcon';
 import BucketStateItem from '@/components/BucketStateItem';
+import SelectItem from '@/components/SelectItem';
 
 const HomePage = () => {
   const [selected, setSelected] = useState(false);
@@ -8,8 +9,13 @@ const HomePage = () => {
     <div>
       홈 페이지입니다.
       <CallIcon />
-      <BucketStateItem
+      {/* <BucketStateItem
         text="전체"
+        selected={selected}
+        onClick={() => setSelected((v) => !v)}
+      /> */}
+      <SelectItem
+        text="원윤서"
         selected={selected}
         onClick={() => setSelected((v) => !v)}
       />
