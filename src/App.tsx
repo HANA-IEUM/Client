75ffd30544ee693c0b2478aeee7545a4a631ab2a
daehyun <kimdeahyeon5445@gmail.com>
2025-08-26 15:38:40 +0900

    refactor: Remove ButtonExamples import from App.tsx (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import AppRouter from './app/AppRouter';
 import ReactQueryProvider from './app/providers/ReactQueryProvider';
-import ButtonExamples from './components/button/Button.examples';
 
 function App() {
   return (
