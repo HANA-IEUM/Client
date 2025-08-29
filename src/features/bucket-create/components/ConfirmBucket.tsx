5cac6c113df77276184772892de5dc46de72fb11
daehyun03 <105865502+daehyun03@users.noreply.github.com>
2025-08-29 14:58:02 +0900

    feat: 버킷생성 페이지 퍼블리싱 (#45)
    * feat: add bucket creation page and update router (#27)
    
    * feat: implement select bucket category (#27)
    
    * feat: bucket creation page publishing (#27)
    
    * feat: add bottom sheet for bucket creation confirmation (#27)
    
    * feat: add steps for box creation (#27)
    
    * refactor: refactor BucketCreatePage (#27)
    
    * feat: implement bucket creation submission logic (#27)
    
    * feat: improve text formatting (#27)
    
    * feat: 오타 수정 (#27)

---
+++ src/features/bucket-create/components/ConfirmBucket.tsx
@@ -0,0 +1,48 @@
+import { useNavigate } from 'react-router-dom';
+import confettiGif from '@/assets/bucket-detail/confetti.gif';
+import Button from '@/components/button/Button.tsx';
+import type { ConfirmBucketProps } from '@/features/bucket-create/types/types.ts';
+
+// Step 7: 최종 화면
+export const ConfirmBucket = ({ title, onSubmit }: ConfirmBucketProps) => {
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/home');
+    onSubmit();
+  };
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-grow space-y-6 text-left">
+        <p className="font-hana-regular text-3xl mb-3">
+          <span className="font-hana-bold">{title}</span>
+          <br />
+          버킷리스트와
+          <br />
+          박스가 성공적으로
+          <br />
+          등록되었어요
+        </p>
+        <div className="w-80 justify-start font-hana-regular">
+          <span className="text-zinc-800 text-lg">
+            박스를 모두 채우면 다양한{' '}
+          </span>
+          <span className="text-zinc-800 text-lg font-hana-bold">
+            제휴 혜택
+          </span>
+          <span className="text-zinc-800 text-lg">
+            을<br />
+            받을 수 있어요
+          </span>
+        </div>
+        <img src={confettiGif} alt={'축하해요!'} />
+      </div>
+      <Button
+        label="확 인"
+        size="full-lg"
+        font="regular"
+        intent="green"
+        onClick={goHome}
+      />
+    </div>
+  );
+};
---
+++ src/features/bucket-create/components/WhoAndWhat.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef } from 'react';
+import { type InputRef, Switch } from 'antd';
+import SelectItem from '@/components/SelectItem.tsx';
+import Input from '@/components/input/Input.tsx';
+import Button from '@/components/button/Button.tsx';
+import type { WhoAndWhatProps } from '@/features/bucket-create/types/types.ts';
+
+// Step 2: 누구와 무엇을
+export const WhoAndWhat = ({
+  title,
+  setTitle,
+  withFamily,
+  setWithFamily,
+  visible,
+  setVisible,
+  onNext,
+}: WhoAndWhatProps) => {
+  const inputRef = useRef<InputRef>(null);
+  useEffect(() => {
+    if (withFamily !== null) {
+      inputRef.current?.focus();
+    }
+  }, [withFamily]);
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-grow space-y-6 text-left">
+        <p className="text-3xl font-hana-regular">
+          <span className="font-hana-bold">버킷리스트</span>에 대한 정보를
+          <br />
+          입력해 주세요
+        </p>
+
+        <div className="flex gap-15 w-full items-center">
+          <span className="font-hana-regular text-3xl">
+            <span className="font-hana-bold">가족</span>에게 버킷 공개{' '}
+          </span>
+          <Switch
+            checked={visible}
+            onChange={setVisible}
+            style={{
+              backgroundColor: visible ? '#008485' : '#d1d5db',
+              transform: 'scale(1.5)',
+              transformOrigin: 'center',
+              position: 'relative',
+              top: '3px',
+            }}
+          />{' '}
+        </div>
+
+        <div className="w-full">
+          <p className="font-hana-bold text-3xl">1. 누구와</p>
+          <div className="flex gap-2 w-full mt-3">
+            <SelectItem
+              text="혼자"
+              selected={withFamily === false}
+              onClick={() => setWithFamily(false)}
+            />
+            <SelectItem
+              text="함께"
+              selected={withFamily === true}
+              onClick={() => setWithFamily(true)}
+            />
+          </div>
+        </div>
+        {withFamily !== null ? (
+          <>
+            <p className="font-hana-bold text-3xl">2. 무엇을</p>
+            <Input
+              ref={inputRef}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="예: 스위스에서 패러글라이딩"
+              intent="green"
+            />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Button
+        label="다 음"
+        size="full-lg"
+        intent="green"
+        onClick={onNext}
+        disabled={!title}
+      />
+    </div>
+  );
+};
